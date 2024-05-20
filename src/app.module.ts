59a78ecfc4193bbaffca6954ec33684c029d4659
Md. Mottasin Lemon <lmottasin@gmail.com>
2024-05-20 11:52:54 +0600

    user rest api module register & typeorm database connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: '',
+      username: '',
+      entities: [],
+      database: 'nptc',
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
